30bc67e3d14fadf076c891d18be7761ef9c36576
blacklytning <blacklytning@yandex.com>
2025-04-13 15:58:33 +0530

    nvim: Disable bufferline on single file

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,8 +1,12 @@
 return {
-    {
-        "akinsho/bufferline.nvim",
-        version = "v4.9.1",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        opts = {},
-    },
+	{
+		"akinsho/bufferline.nvim",
+		version = "v4.9.1",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		opts = {
+			options = {
+				always_show_bufferline = false,
+			},
+		},
+	},
 }
