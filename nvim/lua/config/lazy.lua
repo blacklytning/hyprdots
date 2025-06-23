2c75ddde34e40718a6baf0c3d6573a03ef76ecd9
blacklytning <blacklytning@yandex.com>
2025-06-23 22:33:53 +0530

    nvim: Turn off word wrap by default

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,17 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -25,10 +25,11 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
+vim.o.wrap = false
 
 require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    checker = { enabled = true },
+	spec = {
+		{ import = "plugins" },
+	},
+	checker = { enabled = true, notify = false },
 })
