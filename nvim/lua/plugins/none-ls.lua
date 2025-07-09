ca8d65c4bb60526e03bb73eadf6572cc129fc84c
blacklytning <blacklytning@yandex.com>
2025-07-09 12:26:54 +0530

    nvim: Fetch deprecated eslint_d package from extras

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,23 +1,31 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		opts = {},
-		config = function()
-			local null_ls = require("null-ls")
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvimtools/none-ls-extras.nvim",
+        },
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
-					null_ls.builtins.formatting.prettierd.with({
-						extra_args = { "--tab-width", "4" },
-					}),
-					null_ls.builtins.diagnostics.eslint_d,
-					null_ls.builtins.completion.spell,
-				},
-			})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-	},
+        config = function()
+            local null_ls = require("null-ls")
+
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                    null_ls.builtins.formatting.prettierd.with({
+                        extra_args = { "--tab-width", "4" },
+                    }),
+
+                    require("none-ls.diagnostics.eslint_d"), -- diagnostics
+                    -- require("none-ls.code_actions.eslint_d"),  -- (optional) quick‑fixes
+                    -- require("none-ls.formatting.eslint_d"),    -- (optional) formatting
+
+                    null_ls.builtins.completion.spell,
+                },
+            })
+
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
 }
