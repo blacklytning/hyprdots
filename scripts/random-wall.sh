bc50453e9cd1d2284d7448892184341331cc0a67
blacklytning <blacklytning@yandex.com>
2026-06-17 22:04:42 +0530

    scripts: Add .jpeg support to wallpaper script

--- scripts/random-wall.sh
+++ scripts/random-wall.sh
@@ -3,7 +3,7 @@
 WALLPAPER_DIR="$HOME/.config/walls"
 CACHE_DIR="$HOME/.cache"
 LAST_USED="$CACHE_DIR/last_wall.txt"
-CONFIG="$HOME/.config/hypr/hyprpaper.conf"
+CURRENT_WALL="$CACHE_DIR/current_wall"
 MONITOR="eDP-1"
 
 mkdir -p "$CACHE_DIR"
@@ -14,15 +14,15 @@ LAST=""
 
 # Pick random wallpaper (no immediate repeat)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \
-  \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.webp' \) \
+  \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' -o -iname '*.webp' \) \
   ! -path "$LAST" | shuf -n 1)
 
 [[ -z "$WALLPAPER" ]] && WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 echo "$WALLPAPER" > "$LAST_USED"
 
-ln -sf "$WALLPAPER" "$HOME/.cache/current_wall.jpg"
+ln -sf "$WALLPAPER" "$CURRENT_WALL"
 
 hyprctl hyprpaper unload all
-hyprctl hyprpaper preload "$HOME/.cache/current_wall.jpg"
-hyprctl hyprpaper wallpaper "eDP-1,$HOME/.cache/current_wall.jpg"
+hyprctl hyprpaper preload "$CURRENT_WALL"
+hyprctl hyprpaper wallpaper "$MONITOR,$CURRENT_WALL"
